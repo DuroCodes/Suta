93ab71fb53d06d184dbfd1e944b174729779999d
Duro <davidwright13503@gmail.com>
2022-04-18 19:02:04 -0400

    Fix eval again 📦

--- src/commands/utility/eval.ts
+++ src/commands/utility/eval.ts
@@ -31,7 +31,7 @@ export class UserCommand extends Command {
     const code = interaction.options.get('code')?.value as string;
     try {
       // eslint-disable-next-line no-eval
-      const result: string = eval(`(async () => {\n${code}\n})()`);
+      const result: string = eval(code);
       interaction.reply({
         embeds: [
           new MessageEmbed()

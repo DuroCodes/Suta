7e4f01fe52f4600276d1fd6b28e8787d3e606531
Duro <davidwright13503@gmail.com>
2022-05-10 19:06:00 -0400

    BIG UPDATE!!!

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,15 +1,11 @@
 import { container } from '@sapphire/framework';
-import { bold } from 'chalk';
+import fastifyStatic from '@fastify/static';
 import fastify from 'fastify';
-import { client } from '../main';
+import { bold } from 'chalk';
 import { env } from '../lib';
+import { client } from '../main';
 import guildSchema from '../schemas/guild';
 import { GuildSchema } from '../typings/guild';
-
-interface Data {
-  servers: number;
-  users: number;
-}
 
 interface TranscriptData {
   guildId: string;
@@ -17,16 +13,23 @@ interface TranscriptData {
 }
 
 export default async () => {
-  const app = fastify();
+  const app = fastify({ logger: false });
   const port = env.PORT || 3000;
 
-  app.get('/', async (_req, rep) => {
+  app.register(fastifyStatic, {
+    root: `${__dirname}/public`,
+    prefix: '/public/',
+  });
+
+  app.get('/', async (_req, rep) => rep.sendFile('index.html'));
+
+  app.get('/api/', async (_req, rep) => {
     rep.header('Access-Control-Allow-Origin', '*');
     rep.type('application/json').code(200);
     return {
       servers: client.guilds.cache.size,
       users: client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0),
-    } as Data;
+    };
   });
 
   app.get('/transcripts/:channelId', async (req, rep) => {
---
+++ src/server/public/js/index.js
@@ -0,0 +1,12 @@
+(async () => {
+  fetch('http://suta.tk/api/').then((response) => response.json())
+    .then((data) => {
+      const { servers, users } = data;
+
+      const serversDOM = document.getElementById('servers');
+      const usersDOM = document.getElementById('users');
+
+      serversDOM.innerText = servers || 0;
+      usersDOM.innerText = users || 0;
+    });
+})();

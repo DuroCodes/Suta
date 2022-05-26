e5d74b17ed46c229e355e0c37213484f59412724
Duro <davidwright13503@gmail.com>
2022-05-26 16:31:00 -0400

    Transcript Route Update 👩

--- src/server/routes/transcripts.ts
+++ src/server/routes/transcripts.ts
@@ -1,6 +1,7 @@
 import { Route } from '../../lib';
 import GuildSchema from '../../typings/guild';
 import guildSchema from '../../schemas/guild';
+import { Transcript } from '../../typings/transcript';
 
 export interface TranscriptData {
   channelId: string;
@@ -13,14 +14,14 @@ export default new Route({
       rep.header('Access-Control-Allow-Origin', '*');
       const data: GuildSchema[] = await guildSchema.find();
       const { channelId } = req.params as TranscriptData;
+
       if (!channelId) return rep.code(400).send({ error: 'Missing query parameters. The channelId parameter is required. Example: /transcripts/<channelId>' });
       if (!data.length) return rep.code(404).send({ error: 'No data found' });
 
-      const transcript = data.map(
-        ({ transcripts }) => transcripts?.find(({ name }) => name === channelId),
-      )[0];
-
-      console.log(data);
+      const transcript = (data
+        .map(({ transcripts }) => transcripts)
+        .flat() as Transcript[])
+        .find(({ name: id }) => id === channelId);
 
       if (!transcript) return rep.code(404).send({ error: 'Channel not found' });
 
